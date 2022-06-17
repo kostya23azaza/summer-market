191eda379a1446e62d1e9e05e02988eb86223509
Konstantin Ivanov <kostya-ivanov9623@mail.ru>
2022-06-17 15:31:28 +0300

    проверили гит)

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -3,7 +3,13 @@ insert into categories (title)
 values
 ('Food');
 
-create table products (id bigserial primary key, title varchar(255), price int, category_id bigint references categories (id));
+create table products
+(
+id bigserial primary key,
+title varchar(255),
+price int,
+category_id bigint references categories (id)
+);
 insert into products (title, price, category_id)
 values
 ('Milk', 85, 1),
